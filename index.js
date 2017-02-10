efb6f3ca401e2414bcee45fbf516468afd1c3a91
sagerioz <ericaepperson27@yahoo.com>
2017-02-10 12:12:07 -0700

    got it all in an event object

--- index.js
+++ index.js
@@ -1,37 +1,81 @@
 (function() {
   'use strict';
+var controls = document.getElementById('controls');
+
+var go = document.getElementById('goButton');
+var stopGreen = document.getElementById('goLight')
+
 var stop = document.getElementById('stopButton');
-var stopRed = document.getElementById('stopLight')
-stop.addEventListener('click', function(){
-  stopRed.classList.toggle('stop');
-  console.log("Red bulb on");
+var stopRed = document.getElementById('stopLight');
+
+var slow = document.getElementById('slowButton');
+var stopOrnge = document.getElementById('slowLight');
+// stop.addEventListener('click', function(){
+//   stopRed.classList.toggle('stop');
+//   stopOrnge.classList.remove('slow');
+//   stopGreen.classList.remove('go');
+//   console.log("Red bulb on");
+//   })
+//   //-------------------------
+//
+//   slow.addEventListener('click', function(){
+//     stopOrnge.classList.toggle('slow');
+//     stopRed.classList.remove('stop');
+//     stopOrnge.classList.remove('go');
+//     console.log("Orange bulb on");
+//
+//   })
+//   //-------------green
+//
+//   go.addEventListener('click', function(){
+//     stopGreen.classList.toggle('go');
+//     stopRed.classList.remove('stop');
+//     stopOrnge.classList.remove('slow');
+//     console.log("Green bulb on");
+//   })
+  //-----------------------------
+  controls.addEventListener("mouseover", function(event) {
+    if (event.target.id === "stopButton" || event.target.id === "slowButton" || event.target.id === "goButton") {
+      console.log("Enter " + event.target.innerText + " button!");
+    }
   })
-  //-------------------------
-  var slow = document.getElementById('slowButton');
-  var stopOrnge = document.getElementById('slowLight')
-  slow.addEventListener('click', function(){
+  // YOUR CODE HERE
+//-------------------------------------redo--------------------------------------------
+controls.addEventListener("click", function(){
+  console.log("Event target", event.target);
+  if(event.target.id === 'stopButton'){
+    stopRed.classList.toggle('stop');
+    stopOrnge.classList.remove('slow');
+    stopGreen.classList.remove('go');
+    console.log("Red bulb on");
+  }else if(event.target.id === 'slowButton'){
     stopOrnge.classList.toggle('slow');
+    stopRed.classList.remove('stop');
+    stopOrnge.classList.remove('go');
     console.log("Orange bulb on");
 
-  })
-  //------------------------
-  var go = document.getElementById('goButton');
-  var stopGreen = document.getElementById('goLight')
-  go.addEventListener('click', function(){
+  }else if(event.target.id === 'goButton'){
     stopGreen.classList.toggle('go');
+    stopRed.classList.remove('stop');
+    stopOrnge.classList.remove('slow');
     console.log("Green bulb on");
-  })
-  //-----------------------------
-  stop.addEventListener('mouseenter', function(){
-    console.log("Entered stopRed button");
-      })
-  slow.addEventListener('mouseenter', function(){
-      console.log("Entered stopOrnge button");
-    })
-  go.addEventListener('mouseenter', function(){
-      console.log("Entered stopGreen button");
-   })
-  // YOUR CODE HERE
+  }
+})
+controls.addEventListener("mouseover", function(event) {
+  if (event.target.id === "stopButton" || event.target.id === "slowButton" || event.target.id === "goButton") {
+    console.log("Enter " + event.target.innerText + " button!");
+  }
+})
+
+controls.addEventListener("mouseout", function(event) {
+  if (event.target.id === "stopButton" || event.target.id === "slowButton" || event.target.id === "goButton") {
+    console.log("Left " + event.target.innerText + " button!");
+  }
+})
+
+
+
+
 })();
 // document.getElementById("MyElement").classList.add('class');
 //
